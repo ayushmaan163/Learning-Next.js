414b1b3121cfe6911b2cb965483c8d98221cbaa1
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2024-01-07 15:19:24 +0530

    Adding Common Layout for child routes

---
+++ src/app/about/layout.js
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+  return (
+    <div>
+      <h1>Common Layout for About Screen</h1>
+      {children}
+    </div>
+  );
+}
---
+++ src/app/login/layout.js
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+  return (
+    <div>
+      <h1>Common Layout for Login Screen</h1>
+      {children}
+    </div>
+  );
+}
