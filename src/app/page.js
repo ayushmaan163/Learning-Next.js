e37f2856d23ad1b5a83b8074e37d67a106a388ff
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2024-01-10 11:15:00 +0530

    Learning Image Optimization in Next.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
 "use client";
 import Link from "next/link";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Profile from "../../public/vercel.svg";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h2>Image Optimization in Next.js</h2>
+      <Image src={Profile} />
+      <Image
+        src="https://c.wallhere.com/photos/cf/c9/AI_art_gamer_computer_PC_gaming-2211925.jpg!d"
+        width={700}
+        height={400}
+      />
+      <br />
+      <br />
       <h1>Fetching API in Server Component</h1>
       <Link href="/productlist">Go to Product List</Link>
     </main>
