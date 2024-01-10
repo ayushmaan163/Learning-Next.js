b176ab56a52cd63b3bf125d652e7c90981258071
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2024-01-10 11:30:48 +0530

    Learned Font Optimization in Next.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,21 @@
 "use client";
 import Link from "next/link";
 import styles from "./page.module.css";
-import Image from "next/image";
-import Profile from "../../public/vercel.svg";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: "100",
+  size: "20",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h2>Image Optimization in Next.js</h2>
-      <Image src={Profile} />
-      <Image
-        src="https://c.wallhere.com/photos/cf/c9/AI_art_gamer_computer_PC_gaming-2211925.jpg!d"
-        width={700}
-        height={400}
-      />
+      <h1>Font Optimization in Next.js</h1>
+      <h1 className={roboto.className}>Font with Next.js font feature</h1>
+
       <br />
       <br />
       <h1>Fetching API in Server Component</h1>
